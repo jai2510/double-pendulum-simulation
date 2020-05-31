18acbd55501390a96444eaf61ca6469ea08f1214
jaiisrani <55907204+jaiisrani@users.noreply.github.com>
2020-05-31 14:34:14 +0530

    Update CDSSRG.m

--- CDSSRG.m
+++ CDSSRG.m
@@ -1,7 +1,4 @@
-%% Analytical Mechanics
-
 syms theta1(t) x(t) theta2(t) %  d.o.fs
-%syms L(t)
 
 theta1_dot = diff(theta1,t);
 x_dot = diff(x,t);
@@ -17,47 +14,39 @@ theta2_0 = pi/9 ;
 theta1_dot_0=0;
 x_dot_0=0;
 theta2_dot_0=0;
- y0 = [theta1_0,x_0,theta2_0,theta1_dot_0,x_dot_0,theta2_dot_0];
-%y0 = [theta1_0,x_0,theta2_0]%theta1_dot_0,x_dot_0,theta2_dot_0];
+y0 = [theta1_0,x_0,theta2_0,theta1_dot_0,x_dot_0,theta2_dot_0];
 
-%
-KE(t) = 0.5*m1*((l1*theta1_dot)^2+(x_dot)^2+(x*theta1_dot)^2+2*l1*x*(theta1_dot^2)) + 0.5*m2*((l1*theta1_dot)^2+(x_dot)^2+(x*theta1_dot)^2+(l2*theta1_dot)^2+2*(l1*x*(theta1_dot)^2+l1*l2*theta1_dot*theta2_dot*cos(theta1-theta2)+x_dot*l2*theta2_dot*sin(theta1-theta2)+x*l2*theta1_dot*theta2_dot*cos(theta1-theta2)));
 
-PE(t) = 0.5*k*x*x - m1*g*(l1+x)*cos(theta1) - m2*g*(l2*cos(theta2) + (l1+x)*cos(theta1));
+eqn1 = diff(theta1,2)==(-m1*g*((l1)+x)*sin(theta1)-m2*g*((l1)+x)*sin(theta1)-2*(m1)*x*(theta1_dot)*x_dot-2*(m1)*(l1)*x_dot*theta1_dot-2*(m2)*x*theta1_dot*x_dot-(m2)*(l1)*(l2)*(diff(theta2_dot,1)*cos(theta1-theta2)+(theta2_dot^2)*sin(theta1-theta2))-(m2)*(l2)*(x*cos(theta1-theta2)*diff(theta2,2)+x*sin(theta1-theta2)*(theta2_dot)^2))./((m1)*(l1)*(l1)+(m1)*x*x+2*(m1)*(l1)*(x)+(m2)*(l1)*(l1)+(m2)*x*x+2*(m2)*(l1)*x);
+eqn2 = diff(x,2)==(m1*(x+l1)*theta1_dot^2 +m2*(x+l1)*theta1_dot^2 + m2*l2*theta1_dot*theta2_dot*cos(theta1-theta2)-k*x+(m1+m2)*g*cos(theta1)-m2*l2*(diff(theta2_dot,1)*sin(theta1-theta2)+theta2_dot*(theta1_dot-theta2_dot)*cos(theta1-theta2)))/(m1+m2);
+eqn3 = diff(theta2,2)==(-m2*l2*x_dot*theta2_dot*cos(theta1-theta2)-m2*l1*l2*(diff(theta1,2)*cos(theta1-theta2)-sin(theta1-theta2)*theta1_dot^2)-m2*l2*(diff(x_dot,1)*sin(theta1-theta2)+x_dot*cos(theta1-theta2)*(theta1_dot-theta2_dot))-m2*l2*(x_dot*theta1_dot*cos(theta1-theta2)+x*diff(theta1,2)*cos(theta1-theta2)-x*theta1_dot^2*sin(theta1-theta2)))./(m2*l2*l2);
 
-L = KE - PE;
-%
-f1=diff(L,t)./diff(theta1_dot,t);
-f2=diff(L,t)./diff(x_dot,t);
-f3=diff(L,t)./diff(theta2_dot,t);
-%
-% Equations
-eqn1 = diff(f1,t) == diff(KE-PE,t)./diff(theta1,t);
-eqn2 = diff(f2,t) == diff(KE-PE,t)./diff(x,t);
-eqn3 = diff(f3,t) == diff(KE-PE,t)./diff(theta2,t);
-%
 vars = [theta1(t),x(t),theta2(t)];
 
 V = odeToVectorField([eqn1,eqn2,eqn3]);
 
-M = matlabFunction(V,'vars', {'t','Y'});
 
 % interval
 t0=0;
-tf=100;
+tf=2000;
 interval = [t0 tf];
 
 % solutions
 
+M = matlabFunction(V,'vars', {'t','Y'});
+
 ySol = ode45(M,interval,y0);
 
-tValues = linspace(interval(1),interval(2),10000);
-yValues = deval(ySol,tValues,1);
+tValues = linspace(interval(1),interval(2),200);
+yValues1 = deval(ySol(1),tValues,1);
+yValues2 = deval(ySol(1),tValues,2);
+yValues3 = deval(ySol(1),tValues,3);
+yValues4 = deval(ySol(1),tValues,4);
+yValues5 = deval(ySol(1),tValues,5);
+yValues6 = deval(ySol(1),tValues,6);
 
+% uncomment whichever ones you want to plot, from below.
 % plotting
-plot(tValues,yValues)
-
-
-
-
-
+plot(tValues,yValues1)
+%plot(tValues,yValues2)
+%plot(tValues,yValues3)
